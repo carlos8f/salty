6f5813c2aa8726e34b91d44b4eadb1c15bd7d104
Carlos Rodriguez <carlos@s8f.org>
2014-06-23 16:02:51 -0700

    make identities code follow wallets

--- identities_collection.js
+++ identities_collection.js
@@ -4,8 +4,23 @@ module.exports = function (app) {
   app.require('collection');
   return app.collection({
     name: 'identities',
+    save: function (identity, cb) {
+      identity = {
+        id: identity.id,
+        rev: identity.rev,
+        created: identity.created,
+        updated: identity.updated,
+        identity: salty.identity(identity).toString()
+      };
+      cb(null, identity);
+    },
     load: function (identity, cb) {
-      cb(null, salty.identity(identity));
+      var i = salty.identity(identity.identity);
+      Object.keys(i).forEach(function (k) {
+        identity[i] = w[i];
+      });
+      delete identity.identity;
+      cb(null, identity);
     }
   });
 };
