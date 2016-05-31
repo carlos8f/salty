416b7481836404f70d24274f874220b269df91c9
Carlos Rodriguez <carlos@s8f.org>
2016-05-31 16:30:13 -0700

    rewriting tests

--- test.js
+++ test.js
@@ -1,88 +1,113 @@
-assert = require('assert');
-salty = require('./');
-fs = require('fs');
-path = require('path');
-crypto = require('crypto');
-rimraf = require('rimraf');
-child_process = require('child_process');
-https = require('https');
+assert = require('assert')
+salty = require('./')
+fs = require('fs')
+path = require('path')
+crypto = require('crypto')
+rimraf = require('rimraf')
+child_process = require('child_process')
+https = require('https')
 
-tmpDir = path.join(require('os').tmpDir(), Math.random().toString(36).slice(2));
+var tmpDir = path.join(require('os').tmpDir(), Math.random().toString(36).slice(2))
+var BIN = path.join(__dirname, 'salty')
 
-fs.mkdirSync(tmpDir);
+fs.mkdirSync(tmpDir)
 if (!process.env.DEBUG) {
   process.on('exit', function () {
-    rimraf.sync(tmpDir);
-  });
+    rimraf.sync(tmpDir)
+  })
 }
-else console.log('tmpDir', tmpDir);
+else console.log('tmpDir', tmpDir)
 
 describe('tests', function () {
   var p = path.join(tmpDir, 'alice.jpg')
 
-  it('set up wallets', function (done) {
-    child_process.exec('tar -xf ' + path.join(__dirname, 'test-wallets.tar.gz'), {cwd: tmpDir}, function (err, stdout, stderr) {
-      assert.ifError(err)
-      done()
-    })
-  })
-
-  var streamSize = 0
   it('stream fixture', function (done) {
     https.request({
       hostname: 'raw.githubusercontent.com',
       path: '/carlos8f/node-buffet/master/test/files/folder/Alice-white-rabbit.jpg'
     }, function (res) {
       res.pipe(fs.createWriteStream(p))
-      .on('finish', done);
+      .on('finish', done)
     }).end()
-  });
-  it('read stream fixture', function (done) {
-    fs.createReadStream(p)
-      .on('data', function (chunk) {
-        streamSize += chunk.length
-      })
-      .pipe(crypto.createHash('sha1'))
-      .on('data', function (data) {
-        assert.equal(data.toString('hex'), '2bce2ffc40e0d90afe577a76db5db4290c48ddf4');
-        done();
-      });
-  });
-  it('alice encrypts file for bob (cli)', function (done) {
-    var env = {}
-    Object.keys(process.env).forEach(function (k) {
-      env[k] = process.env[k]
-    })
-    env['HOME'] = path.join(tmpDir, 'alice')
-    var proc = child_process.spawn(path.join(__dirname, 'salty'), ['encrypt', '--sign', '--to=Bob', p, p + '.salty'], {env: env})
-    proc.stderr.pipe(process.stderr)
-    proc.stdout.pipe(process.stdout)
-    proc.once('close', function (code) {
-      assert(!code)
-      fs.unlinkSync(p)
-      done()
-    })
-  })
-  it('bob decrypts file from alice (cli)', function (done) {
-    var env = {}
-    Object.keys(process.env).forEach(function (k) {
-      env[k] = process.env[k]
-    })
-    env['HOME'] = path.join(tmpDir, 'bob')
-    var proc = child_process.spawn(path.join(__dirname, 'salty'), ['decrypt', p + '.salty'], {env: env})
-    proc.stderr.pipe(process.stderr)
-    proc.stdout.pipe(process.stdout)
-    proc.once('close', function (code) {
-      assert(!code)
-      done()
-    })
   })
   it('verify stream fixture', function (done) {
     fs.createReadStream(p)
       .pipe(crypto.createHash('sha1'))
       .on('data', function (data) {
-        assert.equal(data.toString('hex'), '2bce2ffc40e0d90afe577a76db5db4290c48ddf4');
-        done();
-      });
-  });
-});
+        assert.equal(data.toString('hex'), '2bce2ffc40e0d90afe577a76db5db4290c48ddf4')
+        done()
+      })
+  })
+
+  it.skip('set up alice', function (done) {
+    // child_process.spawn(BIN, ['init', 'alice'], {cwd: tmpDir})
+  })
+  it.skip('set up bob', function (done) {
+
+  })
+  it.skip('alice pubkey', function (done) {
+
+  })
+  it.skip('alice change password', function (done) {
+
+  })
+  it.skip('bob pubkey', function (done) {
+
+  })
+  it.skip('alice import bob', function (done) {
+
+  })
+  it.skip('alice ls', function (done) {
+
+  })
+  it.skip('alice save', function (done) {
+
+  })
+  it.skip('alice destroy', function (done) {
+
+  })
+  it.skip('alice restore', function (done) {
+
+  })
+  it.skip('alice encrypt for bob (no sign)', function (done) {
+
+  })
+  it.skip('bob decrypt', function (done) {
+
+  })
+  it.skip('alice encrypt for bob (sign)', function (done) {
+
+  })
+  it.skip('bob decrypt', function (done) {
+
+  })
+  it.skip('alice encrypt for bob (armor)', function (done) {
+
+  })
+  it.skip('bob decrypt', function (done) {
+
+  })
+  it.skip('alice encrypt for bob (compose)', function (done) {
+
+  })
+  it.skip('bob decrypt', function (done) {
+
+  })
+  it.skip('alice sign', function (done) {
+
+  })
+  it.skip('bob verify', function (done) {
+
+  })
+
+  /* errors
+    - init over existing dir
+    - import error
+    - restore bad pw
+    - open wallet bad pw
+    - encrypt noent
+    - decrypt noent
+    - decrypt fail (bad hash, bad sig, box open)
+    - verify fail
+  */
+})
